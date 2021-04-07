99847c7e59b7849a14e43454117fb9bb2c0293d1
Diego Pagini <dq@qbkconsulting.com>
2021-04-07 09:17:31 -0300

    useFetch + useCounter

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -10,6 +10,7 @@ export const useFetch = (url) => {
 	});
 
 	useEffect(() => {
+		setstate({ data: null, loading: true, error: null });
 		fetch(url)
 			.then((resp) => resp.json())
 			.then((data) => {
