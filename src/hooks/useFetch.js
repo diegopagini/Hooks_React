daf3901bc4154f8f3644af4ec0e3bb70ab594528
Diego Pagini <dq@qbkconsulting.com>
2021-04-07 08:11:22 -0300

    useFetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+/** @format */
+
+import { useState, useEffect } from 'react';
+
+export const useFetch = (url) => {
+	const [state, setstate] = useState({
+		data: null,
+		loading: true,
+		error: null,
+	});
+
+	useEffect(() => {
+		fetch(url)
+			.then((resp) => resp.json())
+			.then((data) => {
+				setstate({
+					loading: false,
+					error: null,
+					data: data,
+				});
+			});
+	}, [url]);
+
+	return state;
+};
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './components/03-samples/MultipleCustomHooks';
 
-ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'));
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById('root'));
