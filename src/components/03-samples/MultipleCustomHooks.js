c2f7eb4f2bb93546a6d077f50100a5b9bbfb3f29
Diego Pagini <dq@qbkconsulting.com>
2021-04-07 09:16:31 -0300

    useFetch + useCounter

--- src/components/03-samples/MultipleCustomHooks.js
+++ src/components/03-samples/MultipleCustomHooks.js
@@ -2,14 +2,37 @@
 
 import React from 'react';
 import { useFetch } from '../../hooks/useFetch';
+import { useCounter } from '../../hooks/useCounter';
 import '../02-useEffect/effects.css';
 
 export const MultipleCustomHooks = () => {
-	const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-	console.log(state);
+	const { counter, increment } = useCounter(1);
+
+	const { loading, data } = useFetch(
+		`https://www.breakingbadapi.com/api/quotes/${counter}`
+	);
+
+	// !!data es para preguntar si efectivamente existe la data
+	const { author, quote } = !!data && data[0];
+	console.log(author, quote, counter, increment);
+
 	return (
 		<div>
-			<h1>Custom Hooks</h1>
+			<h1>BreakingBad Qoutes</h1>
+			<hr></hr>
+			{/* ¿Esta cargando? Mostrar Loading... Si termino, mostrar blockquote */}
+			{loading ? (
+				<div className='alert alert-info text-center'>Loading...</div>
+			) : (
+				<blockquote className='blockquote text-end'>
+					<p className='mb-2'>{quote}</p>
+					<footer className='blockquote-footer'>{author}</footer>
+				</blockquote>
+			)}
+
+			<button className='btn btn-primary' onClick={increment}>
+				Next Quote
+			</button>
 		</div>
 	);
 };
