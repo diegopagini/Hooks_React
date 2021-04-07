cc341422fa83e6197e4b09d7dd88fa6874bf7d94
Diego Pagini <dq@qbkconsulting.com>
2021-04-07 07:57:42 -0300

    formulario con custom hook

---
+++ src/components/02-useEffect/FormWithCustomHook.js
@@ -0,0 +1,68 @@
+/** @format */
+
+import React, { useEffect } from 'react';
+import { useForm } from '../../hooks/useForm';
+import './effects.css';
+
+export const FormWithCustomHook = () => {
+	const [formValues, handleInputChange] = useForm({
+		name: '',
+		email: '',
+		password: '',
+	});
+
+	const { name, email, password } = formValues;
+
+	useEffect(() => {
+		console.log('email cambió');
+	}, [email]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formValues);
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<h1>FormWithCustomHook</h1>
+			<hr></hr>
+			<div className='form-group'>
+				<input
+					type='text'
+					name='name'
+					className='form-control'
+					placeholder='Tu nombre'
+					autoComplete='off'
+					value={name}
+					onChange={handleInputChange}
+				></input>
+			</div>
+			<div className='form-group'>
+				<input
+					type='text'
+					name='email'
+					className='form-control'
+					placeholder='tuemail@algo.com'
+					autoComplete='off'
+					value={email}
+					onChange={handleInputChange}
+				></input>
+			</div>
+			<div className='form-group'>
+				<input
+					type='password'
+					name='password'
+					className='form-control'
+					placeholder='*******'
+					autoComplete='off'
+					value={password}
+					onChange={handleInputChange}
+				></input>
+			</div>
+
+			<button type='submit' className='btn btn-primary'>
+				Guardar
+			</button>
+		</form>
+	);
+};
---
+++ src/hooks/useForm.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import { useState } from 'react';
+
+export const useForm = (initialState = {}) => {
+	const [values, setValues] = useState(initialState);
+
+	const handleInputChange = ({ target }) => {
+		setValues({
+			...values,
+			[target.name]: target.value,
+		});
+	};
+
+	return [values, handleInputChange];
+};
