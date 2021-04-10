d8932c01bab5a6845c457d12d714f88110fde061
Diego Pagini <dq@qbkconsulting.com>
2021-04-10 15:44:06 -0300

    idea general de un reducer

---
+++ src/components/08-useReducer/intro-reducer.js
@@ -0,0 +1,36 @@
+/** @format */
+
+// Estado Inicial
+const initialState = [
+	{
+		id: 1,
+		todo: 'Comprar Leche',
+		done: false,
+	},
+];
+
+// Reducer
+const todoReducer = (state = initialState, action) => {
+	if (action?.type === 'agregar') {
+		return [...state, action.payLoad];
+	}
+	return state;
+};
+let todos = todoReducer();
+
+// Nuevo Estado
+const newTodo = {
+	id: 2,
+	todo: 'Comprar Carne',
+	done: false,
+};
+
+// Accion
+const agregarTodoAction = {
+	type: 'agregar',
+	payLoad: newTodo,
+};
+
+todos = todoReducer(todos, agregarTodoAction);
+
+console.log(todos);
