3a6fe53fb12d731ab005faa095e7ce1e54f2f7b3
Diego Pagini <dq@qbkconsulting.com>
2021-04-13 08:07:48 -0300

    build && deploy

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ import ReactDOM from 'react-dom';
 // // import { CallbackHook } from './components/06-useMemos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
 // import './components/08-useReducer/intro-reducer';
-// import { TodoApp } from './components/08-useReducer/TodoApp';
-import { MainApp } from './components/09-useContext/MainApp';
+import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { MainApp } from './components/09-useContext/MainApp';
 
-ReactDOM.render(<MainApp />, document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
